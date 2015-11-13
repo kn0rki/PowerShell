98cfc25f5c437ceb9c224a60ae2bbc0faf81ae86
Kn0rki <vipklick@gmail.com>
2015-11-13 18:18:04 +0100

    initial commit
    Gets specific user information from all childdomains in an active
    directory

---
+++ get-ChildDomainAdUser.ps1
@@ -0,0 +1,6 @@
+#get all users from entire active directory. Includes childdomains
+#gets all childdomains
+$childdomains = (get-addomain).childdomains
+
+#equests user informantion givenname, surname, telephonenumber, emailaddress, country and canonicalname from each childdomain-
+$childdomains | %{echo "Starting for Domain $_; get-aduser -server $_ -filter * -properties * | select-object Givenname, Surname, telephonenumber, emailaddress, country, canonicalname"}
