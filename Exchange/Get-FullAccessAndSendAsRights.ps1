336050aa79d3d23eb225fb4881cd201e7f896a33
Kn0rki <vipklick@gmail.com>
2015-11-15 21:20:21 +0100

    initial release for file

---
+++ Exchange/Get-FullAccessAndSendAsRights.ps1
@@ -0,0 +1,16 @@
+﻿#Exchange get-FullAcessSendAsRights
+#Gets the Accounts with FullAccess / SendAS Rights for each mailbox
+
+$mailboxes = get-mailbox
+$mailboxes |% {
+	echo "USER:$_ -----"
+	ADPERMS = $_ |get-adpermission |?{ $_.ExtendedRights.RawIdentity -eq "Send-As"}
+	$MAILBOXPERMS = $_ | Get-MailboxPermission |? {$_.AccessRights -eq "FullAccess"}
+	
+	echo "ADPERMS: $_ -----"
+	$ADPERMS |ft -autosize
+	
+	echo "MAILBOXPERMS:$_ -----"
+	$MAILBOXPERMS | ft -autosize
+	echo "/USER:$_ -----"
+}
