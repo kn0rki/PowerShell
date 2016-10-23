8c6c49f785859447d8958d233e38884a4555f8e4
Sascha Hoppe <vipklick@gmail.com>
2016-10-23 17:29:06 +0200

    initial import

---
+++ vmware/get-VMsinVeeamBackupjobs.ps1
@@ -0,0 +1,13 @@
+﻿#
+$path = "$env:temp\veeam"
+mkdir $path -ErrorAction SilentlyContinue
+cd $path
+
+Get-VBRJob | ForEach-Object -process {
+        $file = $_.name
+        $file += ".txt"
+        $file =  New-Item -Path $file
+        $_.getobjectsinjob() | ForEach-Object { 
+                add-content -Path $file  -Value $_.Name
+        }
+}
